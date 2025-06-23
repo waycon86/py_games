59ef194e28383d2ee1a5a5d65be64e6faba8aa69
waycon86 <conwaymbenson@gamil.com>
2025-06-23 11:38:45 -0400

    Initial commit: creepy greeting script

---
+++ hello.py
@@ -0,0 +1,19 @@
+import time
+from colorama import Fore, Style, init
+int()
+
+name = input("What is your name? ")
+
+print(Fore.RED +"\n...\n" + Style.RESET_ALL)
+time.sleep(1)
+
+print(Fore.RED + f"Hello, {name}..." + Style.RESET_ALL)
+time.sleep(2)
+
+print(Fore.RED + "\nYou're in my world now."+ Style.RESET_ALL)
+time.sleep(1)
+
+print(Fore.RED +" \nForever." + Style.RESET_ALL)
+
+
+print(f"Hello, {name}! You're in my world now! ")
